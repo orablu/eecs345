4d3d739f709bd846cb59292addd2fbafe052a8f7
Wes Rupert <ora@outlook.com>
2013-03-05 18:29:47 -0500

    Implemented non-cps methods

--- pr02/pr02.scm
+++ pr02/pr02.scm
@@ -10,24 +10,36 @@
   (lambda (e n k)
     (cond
       ((< n 1) (k '()))
-      (else (duplicate e (- n 1) (lambda (v) (cons e k))))
+      (else (duplicate e (- n 1) (lambda (v) (k (cons e v)))))
       )))
 
 ; 2. A function removedups that takes a list and removes any atom that is a repeat of the atom that immediately precedes it.
 (define removedups
   (lambda (l)
     (cond
+      ((null? l) '())
+      ((null? (cdr l)) l)
+      ((eq? (car l) (car (cdr l))) (removedups (cdr l)))
+      (else (cons (car l) (removedups (cdr l))))
       )))
 
 (define removedups-cps
   (lambda (l k)
     (cond
+      ((null? l) (k '()))
+      ((null? (cdr l)) (k l))
+      ((eq (car l) (car (cdr l))) (removedups-cps (cdr l) (k (cdr l))))
+      (else (removedups-cps (cdr l) (lambda (v) (k (cons (car l) v)))))
       )))
 
 ; 3. The function count* that takes a list and and an element and returns the number of occurrences of the element in the list and all its sublists.
 (define count*
   (lambda (a l)
     (cond
+      ((null? l) 0)
+      ((list? (car l)) (+ (count* (car l)) (count* (cdr l))))
+      ((eq? a (car l)) (+ 1 (count* (cdr l))))
+      (else (count* (cdr l)))
       )))
 
 (define count-cps*
@@ -36,9 +48,12 @@
       )))
 
 ; 4. The function numbersonly? that takes a list and returns if the list contains only numbers.
-(define nmbersonly?
+(define numbersonly?
   (lambda (l)
     (cond
+      ((null? l) #t)
+      ((number? (car l)) (numbersonly? (cdr l)))
+      (else #f)
       )))
 
 (define numbersonly-cps?
@@ -50,6 +65,9 @@
 (define cleannumbers
   (lambda (l)
     (cond
+      ((null? l) '())
+      ((numbersonly? (car l)) (cons (car l) (cleannumbers (cdr l))))
+      (else (cleannumbers (cdr l)))
       )))
 
 (define cleannumbers-cps
@@ -62,6 +80,10 @@
 (define merge
   (lambda (l1 l2)
     (cond
+      ((null? l1) l2)
+      ((null? l2) l1)
+      ((< (car l1) (car l2)) (cons (car l1) (merge (cdr l1) l2)))
+      (else (cons (car l2) (merge l1 (cdr l2))))
       )))
 
 (define merge-cps
@@ -74,6 +96,9 @@
 (define evens
   (lambda (b l)
     (cond
+      ((null? l) '())
+      ((not b) (cons (car l) (evens (not b) (cdr l))))
+      (else (evens (not b) (cdr l)))
       )))
 
 (define evens-cps
@@ -86,6 +111,8 @@
 (define Mergesort
   (lambda (l)
     (cond
+      ((null? l) '())
+      (else (merge (Mergesort (evens #t l)) (Mergesort (evens #f l))))
       )))
 
 (define Mergesort-cps
@@ -95,20 +122,19 @@
 
 
 ; 9. Use continuation passing style to create the following function without using external helper functions and without adding new parameters. The function split takes a list and returns a list containing two sublists, the first with the elements at the even indices and the second with the elements at the odd indices:
-(define split
-  (lambda (l)
-    (cond
-      )))
+(define split (lambda (l) (cons (evens #t l) (cons (evens #f l) '()))))
 
 (define split-cps
-  (lambda (l k)
-    (cond
-      )))
+  (lambda (l)
+    (lambda (l k)
+      ; TODO: Wtf do I do with this?
+      l (lambda (v) v))))
 
 
 ; 10. Write the following function without external helper functions or additional parameters. You do not need to use continuation passing style, but you may use continuations or call-with-current-continuation to assist you. The function suffix takes an atom and a list and returns a list containing all elements that occur after the last occurrence of the atom.
 (define suffix
   (lambda (a l)
     (cond
+      ; TODO: Wtf do I do with this?
       )))
 
