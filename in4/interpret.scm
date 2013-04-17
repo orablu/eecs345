eaa0074a8d62434e4377baefe0d97078d664b85f
orablu <ora@outlook.com>
2013-04-17 18:25:38 -0400

    Add empty class support

--- in4/interpret.scm
+++ in4/interpret.scm
@@ -18,16 +18,23 @@
 
 ; Interprets a file and returns the result.
 (define interpret (lambda (file)
-      (unparse (interpret_funcall '(funcall main) (interpret_global (parser file) (newenv))))
-      ))
+    (let ((main (box '())))
+      (unparse (interpret_funcall main (interpret_global (parser file) (newenv) main)))
+      )))
 
-(define interpret_global (lambda (parsetree env)
-    (interpret_statement_list
-      parsetree
-      env
-      (lambda (v) (error "Illegal return"))
-      (lambda (v) (error "Illegal break"))
-      (lambda (v) (error "Illegal continue"))
+(define interpret_global (lambda (parsetree env main)
+    (if (null? parsetree) env
+      (interpret_global (cdr parsetree) (interpret_global_statement (car parsetree) env main) main)
+        )))
+
+(define interpret_global_statement (lambda (stmt env main)
+    (cond
+      ((eq? 'var             (op stmt)) (interpret_declare  stmt env               )) ; TODO: Change
+      ((eq? 'static-var      (op stmt)) (interpret_sdeclare stmt env               )) ; TODO: Add
+      ((eq? 'function        (op stmt)) (interpret_fundef   stmt env               )) ; TODO: Change
+      ((eq? 'static-function (op stmt)) (interpret_sfundef  stmt env               )) ; TODO: Add
+      ((eq? 'class           (op stmt)) (interpret_classdef stmt env               )) ; TODO: Add
+      (else (error "Action not supported in global scope"))
       )))
 
 ; Returns the value to human-readable format.
@@ -48,14 +55,48 @@
 ; Interprets a single statement.
 (define interpret_statement (lambda (stmt env ret break cont)
     (cond
-      ((eq? '=        (op stmt)) (interpret_assign  stmt env               ))
-      ((eq? 'begin    (op stmt)) (interpret_begin   stmt env ret break cont))
-      ((eq? 'if       (op stmt)) (interpret_if      stmt env ret break cont))
-      ((eq? 'while    (op stmt)) (interpret_while   stmt env ret           ))
-      ((eq? 'break    (op stmt)) (break                  env               ))
-      ((eq? 'continue (op stmt)) (cont                   env               ))
-      ((eq? 'return   (op stmt)) (ret (interpret_value (op1 stmt) env)     ))
-      (else                      (interpret_value   stmt env               ))
+      ((eq? '=               (op stmt)) (interpret_assign   stmt env               ))
+      ((eq? 'begin           (op stmt)) (interpret_begin    stmt env ret break cont))
+      ((eq? 'if              (op stmt)) (interpret_if       stmt env ret break cont))
+      ((eq? 'while           (op stmt)) (interpret_while    stmt env ret           ))
+      ((eq? 'var             (op stmt)) (interpret_declare  stmt env               ))
+      ((eq? 'static-var      (op stmt)) (interpret_sdeclare stmt env               ))
+      ((eq? 'function        (op stmt)) (interpret_fundef   stmt env               ))
+      ((eq? 'static-function (op stmt)) (interpret_sfundef  stmt env               ))
+      ((eq? 'class           (op stmt)) (interpret_classdef stmt env               ))
+      ((eq? 'break           (op stmt)) (break                   env               ))
+      ((eq? 'continue        (op stmt)) (cont                    env               ))
+      ((eq? 'return          (op stmt)) (ret (interpret_value (op1 stmt) env)      ))
+      (else (interpret_value stmt env))
+      )))
+
+; Interprets the value of a mathematical statement.
+(define interpret_value (lambda (stmt env)
+    (cond
+      ((null? stmt) env)
+      ((number? stmt) stmt)
+      ((eq? 'true  stmt) #t)
+      ((eq? 'false stmt) #f)
+      ((not (list? stmt)) (lookup stmt env))
+      ((null? (cdr stmt)) (interpret_value (car stmt) env))
+      ((eq? 'funcall  (op stmt)) (interpret_funcall stmt env))
+      ((eq? 'dot (op stmt)) (interpret_dot stmt env)) ; TODO: Add
+      ((and (eq? '- (op stmt)) (null? (op2 stmt))) (-                  (interpret_value (op1 stmt) env)))
+      ((eq? '+  (op stmt)) (+         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '-  (op stmt)) (-         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '*  (op stmt)) (*         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '/  (op stmt)) (quotient  (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '%  (op stmt)) (remainder (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '== (op stmt)) (eq?       (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '!= (op stmt)) (not (=    (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env))))
+      ((eq? '<  (op stmt)) (<         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '>  (op stmt)) (>         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '<= (op stmt)) (<=        (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '>= (op stmt)) (>=        (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '&& (op stmt)) (and       (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '|| (op stmt)) (or        (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
+      ((eq? '!  (op stmt)) (not       (interpret_value (op1 stmt) env)                                 ))
+      (else  (error "Symbol not recognized:" (op stmt) '-> stmt))
       )))
 
 ; Interprets an assignment (e.g. "x = 10;").
@@ -76,11 +117,47 @@
           (cont (popframe v)))
         ))))
 
+; Interprets an if statement (e.g. "if (...) ...;" or "if (...) {...} else {...}").
+(define interpret_if (lambda (stmt env ret break cont)
+    (cond
+      ((interpret_value (op1 stmt) env) (interpret_statement (op2 stmt) env ret break cont))
+      ((null? (op3 stmt)) env)
+      (else (interpret_statement (op3 stmt) env ret break cont))
+      )))
+
+; Interprets the value of a while loop.
+(define interpret_while (lambda (stmt env ret)
+    (call/cc (lambda (break)
+      (letrec ((loop (lambda (test body env)
+          (if (interpret_value test env)
+            (loop test body (call/cc (lambda (cont) (interpret_statement body env ret break cont))))
+            env))))
+          (loop (op1 stmt) (op2 stmt) env)))
+        )))
+
 ; Interprets a declaration (e.g. "var x;" or "var y = 10").
 (define interpret_declare (lambda (stmt env)
+    (error "Not Implemented") ; TODO: Implement instance variable declare.
+    ))
+
+; Interprets a declarations of a static variable (e.g. "static var x;" or "static var y = 10").
+(define interpret_sdeclare (lambda (stmt env)
     (if (null? (op2 stmt))
       (declare (op1 stmt) env)
       (assign (op1 stmt) (interpret_value (op2 stmt) env) (declare (op1 stmt) env))
+      )))
+
+; Interprets a function definition (e.g. "main() {...}").
+(define interpret_fundef (lambda (stmt env)
+    (error "Not Implemented") ; TODO: Implement instance method declare.
+    ))
+
+; Interprets a static function definition (e.g. "static main() {...}").
+(define interpret_sfundef (lambda (stmt env)
+    (assign
+      (op1 stmt)
+      (list (op2 stmt) (op3 stmt) (lambda () (pushframe env)))
+      (declare (op1 stmt) env)
       )))
 
 ; Interprets a function call (e.g. "min(3, 5)").
@@ -96,14 +173,6 @@
         ret)
       ))))
 
-; Interprets a function definition (e.g. "main() {...}").
-(define interpret_fundef (lambda (stmt env)
-    (assign
-      (op1 stmt)
-      (list (op2 stmt) (op3 stmt) (lambda () (pushframe env)))
-      (declare (op1 stmt) env)
-      )))
-
 (define interpret_function (lambda (stmt env ret)
     (popframe
       (interpret_statement_list
@@ -112,54 +181,6 @@
         ret
         (lambda (v) (error "Illegal break"))
         (lambda (v) (error "Illegal continue")))
-        )))
-
-; Interprets an if statement (e.g. "if (...) ...;" or "if (...) {...} else {...}").
-(define interpret_if (lambda (stmt env ret break cont)
-    (cond
-      ((interpret_value (op1 stmt) env) (interpret_statement (op2 stmt) env ret break cont))
-      ((null? (op3 stmt)) env)
-      (else (interpret_statement (op3 stmt) env ret break cont))
-      )))
-
-; Interprets the value of a mathematical statement.
-(define interpret_value (lambda (stmt env)
-    (cond
-      ((null? stmt) '())
-      ((number? stmt) stmt)
-      ((eq? 'true  stmt) #t)
-      ((eq? 'false stmt) #f)
-      ((not (list? stmt)) (lookup stmt env))
-      ((null? (cdr stmt)) (interpret_value (car stmt) env))
-      ((eq? 'function (op stmt)) (interpret_fundef  stmt env               ))
-      ((eq? 'funcall  (op stmt)) (interpret_funcall stmt env               ))
-      ((eq? 'var      (op stmt)) (interpret_declare stmt env               ))
-      ((and (eq? '- (op stmt)) (null? (op2 stmt))) (-                  (interpret_value (op1 stmt) env)))
-      ((eq? '+  (op stmt)) (+         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '-  (op stmt)) (-         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '*  (op stmt)) (*         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '/  (op stmt)) (quotient  (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '%  (op stmt)) (remainder (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '== (op stmt)) (eq?       (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '!= (op stmt)) (not (=    (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env))))
-      ((eq? '<  (op stmt)) (<         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '>  (op stmt)) (>         (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '<= (op stmt)) (<=        (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '>= (op stmt)) (>=        (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '&& (op stmt)) (and       (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '|| (op stmt)) (or        (interpret_value (op1 stmt) env) (interpret_value (op2 stmt) env)))
-      ((eq? '!  (op stmt)) (not       (interpret_value (op1 stmt) env)                                 ))
-      (else  (error "Symbol not recognized:" (op stmt) '-> stmt))
-      )))
-
-; Interprets the value of a while loop.
-(define interpret_while (lambda (stmt env ret)
-    (call/cc (lambda (break)
-      (letrec ((loop (lambda (test body env)
-          (if (interpret_value test env)
-            (loop test body (call/cc (lambda (cont) (interpret_statement body env ret break cont))))
-            env))))
-          (loop (op1 stmt) (op2 stmt) env)))
         )))
 
 ; Sets the formal parameters of the environment to the values in the given parameters.
